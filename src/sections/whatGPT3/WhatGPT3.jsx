00950d2a4aa0909d53eba13debad6cd1d5e5f51c
amirpourastarabadi <amirpourastarabadi@gmail.com>
2023-09-10 19:41:06 +0330

    WIP wgpt

--- src/sections/whatGPT3/WhatGPT3.jsx
+++ src/sections/whatGPT3/WhatGPT3.jsx
@@ -1,7 +1,27 @@
+import { Feature } from "../../components"
 import "./whatGPT3.css"
 
 const WhatGPT3 = () => {
-  return <div>WhatGPT3</div>
+  return (
+    <div className="gpt3__whatgpt3 section__margin" id="wgpt3">
+      <div className="gpt3__whatgpt3-feature">
+        <Feature title="" text="" />
+      </div>
+      <div className="gpt3__whatgpt3-heading">
+        <h1 className="gradient__text">
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Non ut
+          officia dolorem quas at mollitia, accusamus porro! Ipsa, reiciendis
+          ducimus!
+        </h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing.</p>
+      </div>
+      <div className="gpt3__whatgpt3-container">
+        <Feature title="" text="" />
+        <Feature title="" text="" />
+        <Feature title="" text="" />
+      </div>
+    </div>
+  )
 }
 
 export default WhatGPT3
